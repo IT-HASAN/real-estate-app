974f531d10915e7d5b876a59aa2ed65274034a6e
Ishan Hasan <84034201+IT-HASAN@users.noreply.github.com>
2026-03-04 13:57:54 +0000

    Complete listing page

--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -279,7 +279,9 @@ export default function UpdateListing() {
               />
               <div className='flex flex-col items-center'>
                 <p>Regular price</p>
-                <span className='text-xs'>($ / Month)</span>
+                {formData.type === 'rent' && (
+                  <span className='text-xs'>($ / Month)</span>
+                )}
               </div>
             </div>
             {formData.offer && (
@@ -296,7 +298,9 @@ export default function UpdateListing() {
                 />
                 <div className='flex flex-col items-center'>
                   <p>Discounted price</p>
-                  <span className='text-xs'>($ / Month)</span>
+                  {formData.type === 'rent' && (
+                    <span className='text-xs'>($ / Month)</span>
+                  )}
                 </div>
               </div>
             )}
