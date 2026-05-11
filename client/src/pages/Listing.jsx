ec5a9da41796d96b9f8b482762fd4b4ae5a25f0a
Ishan Hasan <84034201+IT-HASAN@users.noreply.github.com>
2026-05-11 15:59:14 +0100

    Create the listing item component and show listings

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -88,11 +88,11 @@ export default function Listing() {
               <p className='bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
                 {listing.type === 'rent' ? 'For Rent' : 'For Sale'}
               </p>
-              {
-                listing.offer && (
-                  <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>{`$${+listing.regularPrice - +listing.discountedPrice} discount`}</p>
-                )
-              }
+              {listing.offer && (
+                <p className='bg-green-900 w-full max-w-[200px] text-white      text-center p-1 rounded-md'>
+                  ${+listing.regularPrice - +listing.discountedPrice} Discount
+                </p>
+              )}
             </div>
             <p className='text-slate-800'>
               <span className='font-semibold text-black'>
